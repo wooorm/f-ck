e0aed2ef62e0b974554b72c84f83e29d53a543f7
Titus Wormer <tituswormer@gmail.com>
2020-10-13 09:56:44 +0200

    Update dev-dependencies

--- test.js
+++ test.js
@@ -3,38 +3,38 @@
 var test = require('tape')
 var fck = require('.')
 
-test('fck', function(t) {
-  t.test('.vowel', function(st) {
-    st.equal(fck.vowel('butt'), 'b*tt')
-    st.equal(fck.vowel('butt', '-'), 'b-tt')
-    st.end()
+test('fck', function (t) {
+  t.test('.vowel', function (t) {
+    t.equal(fck.vowel('butt'), 'b*tt')
+    t.equal(fck.vowel('butt', '-'), 'b-tt')
+    t.end()
   })
 
-  t.test('.inner', function(st) {
-    st.equal(fck.inner('butt'), 'b**t')
-    st.equal(fck.inner('butt', '-'), 'b--t')
-    st.equal(fck.inner('but'), 'b*t')
-    st.equal(fck.inner('bu'), 'bu')
-    st.equal(fck.inner('b'), 'b')
-    st.equal(fck.inner(''), '')
-    st.end()
+  t.test('.inner', function (t) {
+    t.equal(fck.inner('butt'), 'b**t')
+    t.equal(fck.inner('butt', '-'), 'b--t')
+    t.equal(fck.inner('but'), 'b*t')
+    t.equal(fck.inner('bu'), 'bu')
+    t.equal(fck.inner('b'), 'b')
+    t.equal(fck.inner(''), '')
+    t.end()
   })
 
-  t.test('.grawlix', function(st) {
-    st.equal(fck.grawlix('butt'), '@#$%')
-    st.equal(fck.grawlix(4), '@#$%')
-    st.equal(fck.grawlix('buttb'), '@#$%!')
-    st.equal(fck.grawlix('buttbu'), '@#$%!&')
-    st.equal(fck.grawlix('buttbut'), '@#$%!&?')
-    st.equal(fck.grawlix('buttbutt'), '@#$%!&?@')
-    st.equal(fck.grawlix('buttbuttb'), '@#$%!&?@#')
-    st.equal(fck.grawlix('but'), '@#$')
-    st.equal(fck.grawlix('bu'), '@#')
-    st.equal(fck.grawlix('b'), '@')
-    st.equal(fck.grawlix(''), '')
-    st.equal(fck.grawlix('butt', '*'), '****')
-    st.equal(fck.grawlix('butt', '123'), '1231')
-    st.end()
+  t.test('.grawlix', function (t) {
+    t.equal(fck.grawlix('butt'), '@#$%')
+    t.equal(fck.grawlix(4), '@#$%')
+    t.equal(fck.grawlix('buttb'), '@#$%!')
+    t.equal(fck.grawlix('buttbu'), '@#$%!&')
+    t.equal(fck.grawlix('buttbut'), '@#$%!&?')
+    t.equal(fck.grawlix('buttbutt'), '@#$%!&?@')
+    t.equal(fck.grawlix('buttbuttb'), '@#$%!&?@#')
+    t.equal(fck.grawlix('but'), '@#$')
+    t.equal(fck.grawlix('bu'), '@#')
+    t.equal(fck.grawlix('b'), '@')
+    t.equal(fck.grawlix(''), '')
+    t.equal(fck.grawlix('butt', '*'), '****')
+    t.equal(fck.grawlix('butt', '123'), '1231')
+    t.end()
   })
 
   t.end()
