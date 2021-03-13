5fbfb7e022985929c12db786ec6dc70f74f39b0b
Titus Wormer <tituswormer@gmail.com>
2021-03-13 14:36:35 +0100

    Add JSDoc based types

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 import repeat from 'repeat-string'
 
-// `butt` > `b*tt`
+/**
+ * `butt` > `b*tt`
+ *
+ * @param {string} value
+ * @param {string} [character='*']
+ * @returns {string}
+ */
 export function vowel(value, character) {
   return value.replace(/[aeiouy]/g, character || '*')
 }
 
-// `butt` > `b**t`
+/**
+ * `butt` > `b**t`
+ *
+ * @param {string} value
+ * @param {string} [character='*']
+ * @returns {string}
+ */
 export function inner(value, character) {
   if (value.length <= 2) {
     return value
@@ -18,7 +30,13 @@ export function inner(value, character) {
   )
 }
 
-// `butt` > `@#$%`
+/**
+ * `butt` > `@#$%`
+ *
+ * @param {number|string} value
+ * @param {string} [pattern='@#$%!&?']
+ * @returns {string}
+ */
 export function grawlix(value, pattern = '@#$%!&?') {
   var length = typeof value === 'number' ? value : value.length
   return (
