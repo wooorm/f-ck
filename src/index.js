66a26153f90da083c075bb9523d06aa58f5f871a
Titus Wormer <tituswormer@gmail.com>
2024-10-25 12:59:01 +0200

    Rewrite project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 /* eslint-env browser */
 
+import {ok as assert} from 'devlop'
 import * as fck from 'f-ck'
 
 const input = document.querySelector('input')
 const output = document.querySelector('output')
 const select = document.querySelector('select')
+assert(input)
+assert(select)
 
 change()
 
@@ -12,6 +15,10 @@ input.addEventListener('input', change)
 select.addEventListener('change', change)
 
 function change() {
-  const fn = select.selectedOptions[0].textContent
-  output.value = fck[fn](input.value)
+  assert(input)
+  assert(output)
+  assert(select)
+  const name = select.selectedOptions[0].textContent
+  assert(name === 'grawlix' || name === 'inner' || name === 'vowel')
+  output.value = fck[name](input.value)
 }
